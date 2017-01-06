4096cc15c0cd5eae38db1e6570ab4857053e7140
Sardhendu Mishra <sardhendumishra@gmail.com>
2017-01-05 18:41:49 -0600

    adding changes

--- Word-Search-NNets/Word-Nets/model.py
+++ Word-Search-NNets/Word-Nets/model.py
@@ -18,18 +18,24 @@ def reset_graph():  # Reset the graph
 
 # vacab_size = 6
 def dynamic_RNN_model(
-    num_hid_units = 100,
+    num_hid_units = 3,
     vocab_size = 7,
     momentum = 0.9,
-    learning_rate = 0.01
+    learning_rate = 0.001
     ):
+    print ('The num of hidden unit is: ', num_hid_units)
+    print ('The Vocab size is: ', vocab_size)
+    print ('The momentum is: ', momentum)
+    print ('The learning_rate is: ', learning_rate)
+    
     
     num_classes = vocab_size
 
     reset_graph()
     
     x = tf.placeholder(tf.int32, shape = [None, None], name='input_placeholder')
-    y = tf.placeholder(tf.int32, shape = [None, None], name='output_placeholder')
+    y = tf.placeholder(tf.int32, shape = [None, None], name='output_placeholdr')
+    x_lenarr = tf.placeholder(tf.float32, shape = [None], name='output_placeholdr')
     batch_size = tf.shape(x)[0]
     
 
@@ -37,59 +43,80 @@ def dynamic_RNN_model(
     # Creating an Embedding matrix with a random weight for all vacab to hidden_matrix
     embed_to_hid_wghts = tf.get_variable('embedding_matrix', [vocab_size, num_hid_units])
     embed_to_hid_layer = tf.nn.embedding_lookup(embed_to_hid_wghts, x)
+    print ('The shape of embed_to_hid_wghts is: ', embed_to_hid_wghts.get_shape())
+    print ('The shape of embed_to_hid_layer is: ', embed_to_hid_layer.get_shape())
 
 
     # HIDDEN LAYER OPERATION
     rnn_cell = tf.nn.rnn_cell.LSTMCell(num_hid_units, state_is_tuple=True)
     init_state = rnn_cell.zero_state(batch_size, tf.float32)  # Each sequence will hava a state that it passes to its next sequence
-    rnn_outputs, new_state = tf.nn.dynamic_rnn(
-                                        cell=rnn_cell,
-                                        # sequence_length=X_lengths,
-                                        initial_state=init_state,
-                                        inputs=embed_to_hid_layer)
+    rnn_outputs, new_state = tf.nn.dynamic_rnn(cell=rnn_cell,
+                                               sequence_length=x_lenarr,
+                                               initial_state=init_state,
+                                               inputs=embed_to_hid_layer,
+                                               dtype=tf.float32)
     
 
     # OUTPUT LAYER OPERATION
     # Initialize the weight and biases for the output layer. We use variable scope because we would like to share the weights 
     with tf.variable_scope('output_layer'):
-        hid_to_output_wght = tf.get_variable('hid_to_output_wght', 
-                                                 [num_hid_units, num_classes], 
-                                                 initializer = tf.random_normal_initializer())
-        output_bias = tf.get_variable('output_bias',
-                                      [num_classes],
-                                      initializer = tf.random_normal_initializer())
+        hid_to_output_wght = tf.get_variable('hid_to_output_wght', [num_hid_units, num_classes],
+                                            initializer = tf.random_normal_initializer())
+        output_bias = tf.get_variable('output_bias', [num_classes],
+                                      initializer=tf.random_normal_initializer())
     
-    rnn_outputs = tf.reshape(rnn_outputs, [-1, num_hid_units])  
-    hid_to_ouptut_layer = tf.matmul(rnn_outputs, hid_to_output_wght) +  output_bias
+    rnn_outputs = tf.reshape(rnn_outputs, [-1, num_hid_units])
+    hid_to_ouptut_layer = tf.matmul(rnn_outputs, hid_to_output_wght) +  output_bias  
+    # Also use tf.batch_matmul(rnn_outputs, hid_to_output_wght) +  output_bias  
     output_state = tf.nn.softmax(hid_to_ouptut_layer, name=None)
  
     
-    
     # CALCULATING LOSS, OPTIMIZING THE COST FUNCTION, MEASURING ACCURACY
-    loss_CE = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(hid_to_ouptut_layer, tf.reshape(y, [-1])))
+    y_reshaped = tf.reshape(y, [-1])
+    softmax_opt = tf.nn.sparse_softmax_cross_entropy_with_logits(hid_to_ouptut_layer, y_reshaped)
+    loss_CE = tf.reduce_mean(softmax_opt)
     
+    # # MASK THE LOSES
+    mask = tf.sign(tf.to_float(y_reshaped))  
+    masked_loss = mask * loss_CE
+ 
+    # # Bring back to [B, T] shape
+    masked_loss = tf.reshape(masked_loss,  tf.shape(y))
+
+    # # Calculate mean loss
+    mean_loss_by_example = tf.reduce_sum(masked_loss, reduction_indices=1) / x_lenarr
+    mean_loss = tf.reduce_mean(mean_loss_by_example)
+
     # The sparse_softmax uses dtype as int32 or int64
-    optimizer = tf.train.MomentumOptimizer(learning_rate, 
-                                            momentum, 
-                                            use_locking=False, 
-                                            name='Momentum', 
-                                            use_nesterov=True).minimize(loss_CE)
-    # y_ = tf.reshape(y, [-1])
-    # correct_prediction = tf.equal(tf.arg_max(output_state,1), tf.arg_max(y_ ,1))
-    # accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
-    
+
+    # OPTIMIZE
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss_CE)
+#     optimizer = tf.train.MomentumOptimizer(learning_rate, 
+#                                             momentum, 
+#                                             use_locking=False, 
+#                                             name='Momentum', 
+#                                             use_nesterov=True).minimize(loss_CE)
 
     # Returns graph objects
     return dict(
         x=x,
         y=y,
+        x_lenarr=x_lenarr,
         batch_size = batch_size,
         init_state = init_state,
         new_state = new_state,
+        rnn_outputs = rnn_outputs,
+        hid_to_ouptut_layer = hid_to_ouptut_layer,
+        output_state = output_state,
+        softmax_opt = softmax_opt,
         loss_CE = loss_CE,
+        y_reshaped = y_reshaped,
+        mask = mask,
+        masked_loss = masked_loss,
+        mean_loss_by_example = mean_loss_by_example,
+        mean_loss = mean_loss,
         optimizer = optimizer,
         training_prediction = output_state
-        # accuracy = tf.Variable([[1,2,3]])
     )
 
 # Model()
--- Word-Search-NNets/Word-Nets/train-test.py
+++ Word-Search-NNets/Word-Nets/train-test.py
@@ -32,101 +32,132 @@ class Train():
                   / predictions.shape[0])
         
 
-    def train_network(self, graph_dict, num_batches):
+    def train_network(self, graph_dict):
         with tf.Session() as sess:
             sess.run(tf.initialize_all_variables())
             
-            aa = np.array([[1,2,3,4,5], [1,3,4,0,5]])  # , [2,3,4,5,6], [2,1,5,4,3]
-            bb = np.array([[2,3,4,5,2], [3,4,0,5,2]])  # , [3,4,5,6,2], [1,5,4,3,2]
-            cc = np.array([[1,4,2,3,4,0], [1,3,4,3,5,0]])
-            dd = np.array([[4,2,3,4,0,0], [3,4,3,5,0,0]])
+            aa = np.array([[1,2,3,4,5], [1,3,4,0,0]])  # , [2,3,4,5,6], [2,1,5,4,3]
+            bb = np.array([[2,3,4,5,6], [3,4,6,0,0]])  # , [3,4,5,6,2], [1,5,4,3,2]
+            ab = np.array([5,3])
+            cc = np.array([[1,4,2,3,4,5], [1,3,4,0,0,0]])
+            dd = np.array([[4,2,3,4,5,6], [3,4,6,0,0,0]])
+            cd = np.array([6,3])
+
+            # aa = np.array([[1,2,3,4,5], [1,3,4,2,4]])  # , [2,3,4,5,6], [2,1,5,4,3]
+            # bb = np.array([[2,3,4,5,6], [3,4,2,4,6]])  # , [3,4,5,6,2], [1,5,4,3,2]
+            # ab = np.array([5,5])
+            # cc = np.array([[1,4,2,3,4,5], [1,3,4,2,3,4]])
+            # dd = np.array([[4,2,3,4,5,6], [3,4,2,3,4,6]])
+            # cd = np.array([6,6])
 
             ee = np.array([[1,3,2,4,5], [2,3,4,0,5]]) 
             ff = np.array([[3,2,4,5,0], [3,4,0,5,0]])
             epochs = 1
             cdoc = np.random.choice(np.arange(20), 5) # Randomly select group of 5 batches from the cross valid dataset to test after every 20 batches
             for epoch in np.arange(epochs):
-                new_hid_layer_state = None
+                training_loss = None
 #                 print (graph_dict)
+                new_state_ = None
+                for m,n,o in [[aa,bb,ab],[cc,dd,cd]]:#np.arange(2):  # no in np.arange(num_batches):
 
-                for m,n in [[aa,bb],[cc,dd]]:#np.arange(2):  # no in np.arange(num_batches):
-                    # with open(self.train_batch_dir+'batch'+str(no)+'.pickle', 'rb') as f:
-                    #     dataset = pickle.load(f)
-                        
-                        # batch_train_dataset = dataset['batch_train_dataset']
-                        # batch_train_labels = dataset['batch_train_labels']
+                        # print (m)
+                        # print ('')
+                        # print (n)
+                        # print ('')
+                        # print (o)
+                        # print ('')
 
-                        print (m)
-                        print ('')
-                        print (n)
-                        print ('')
                         batch_train_dataset = m
                         batch_train_labels = n
+                        batch_train_lenarr = o
+
+                        feed_dict= {graph_dict['x']: batch_train_dataset, 
+                                    graph_dict['y']: batch_train_labels,
+                                    graph_dict['x_lenarr']: batch_train_lenarr}
+            
+                        if new_state_ is not None:
+                            print ('Using the new RNN State')
+                            feed_dict[graph_dict['init_state']] = new_state_
+
+                        x_, y_, x_lenarr_, batch_size_, init_state_, new_state_, rnn_output_, hid_to_ouptut_layer_, output_state_, softmax_opt_, loss_CE_, y_reshaped_, mask_, masked_loss_, mean_loss_by_example_, mean_loss_, optimizer_, training_prediction_ = \
+                        sess.run([graph_dict['x'], 
+                                    graph_dict['y'], 
+                                    graph_dict['x_lenarr'], 
+                                    graph_dict['batch_size'],
+                                    graph_dict['init_state'],
+                                    graph_dict['new_state'],
+                                    graph_dict['rnn_outputs'],
+                                    graph_dict['hid_to_ouptut_layer'],
+                                    graph_dict['output_state'],
+                                    graph_dict['softmax_opt'],
+                                    graph_dict['loss_CE'],
+                                    graph_dict['y_reshaped'],
+                                    graph_dict['mask'],
+                                    graph_dict['masked_loss'],
+                                    graph_dict['mean_loss_by_example'], 
+                                    graph_dict['mean_loss'],
+                                    graph_dict['optimizer'],
+                                    graph_dict['training_prediction']], 
+                                    feed_dict=feed_dict)
+
+                        # training_loss += loss
 
 
-                        if not new_hid_layer_state: 
-                            print ('Using the Default RNN State')
-                            feed_dict= {graph_dict['x']: batch_train_dataset, 
-                                        graph_dict['y']: batch_train_labels}
-                                        # graph_dict['batch_size']: batch_size}
-                        else:
-                            print ('Using the New RNN State')
-                            feed_dict= {graph_dict['x']: batch_train_dataset, 
-                                        graph_dict['y']: batch_train_labels,
-                                        graph_dict['init_state'] : new_hid_layer_state}
-
-                        bs, nwst, loss, opt, tp = sess.run([graph_dict['batch_size'],
-                                                        graph_dict['new_state'],
-                                                        graph_dict['loss_CE'],
-                                                        graph_dict['optimizer'],
-                                                        graph_dict['training_prediction']], 
-                                                        feed_dict=feed_dict)
-                        new_hid_layer_state = nwst
-
-                        acc = self.accuracy(tp, batch_train_labels)
-
-                        print ('accuracy \n', acc)
+                        # acc = self.accuracy(tp, batch_train_labels)
+                        print ('x = ', x_.shape , '\n',  x_)
                         print ('')
-                        print ('popopopopopopopoop')
+                        print ('y = ', x_.shape, '\n', y_)
+                        print ('')
+                        print ('x_lenarr = ', x_lenarr_)
+                        print ('')
+                        print ('batch_size = \n', batch_size_)
+                        print ('')
+                        print ('init_state = \n', init_state_)
+                        print ('')
+                        print ('new_state = \n',  new_state_)
+                        print ('')
+                        print ('rnn_output = ', rnn_output_.shape, '\n', rnn_output_)
+                        print ('')
+                        print ('hid_to_ouptut_layer = ', hid_to_ouptut_layer_.shape, '\n', hid_to_ouptut_layer_)
+                        print ('')
+                        print ('output_state = ', output_state_.shape, '\n', output_state_)
+                        print ('')
+                        print ('softmax_opt = ', softmax_opt_.shape, '\n', softmax_opt_)
+                        print ('')
+                        print ('loss_CE = ', loss_CE_.shape, '\n', loss_CE_)
+                        print ('')
+                        print ('y_reshaped = ', y_reshaped_.shape, '\n',  y_reshaped_)
+                        print ('')
+                        print ('mask = ', mask_.shape, '\n',  mask_)
+                        print ('')
+                        print ('masked_loss = ', masked_loss_.shape, '\n',  masked_loss_)
+                        print ('')
+                        print ('mean_loss_by_example = ', mean_loss_by_example_.shape, '\n', mean_loss_by_example_)
+                        print ('')
+                        print ('mean_loss = ', mean_loss_)
+                        print ('')
+                        print ('training_prediction = \n', training_prediction_)
+                        print ('')
+                        print ('')
+                        print ('popopopopopopopooppopopopopopopopooppopopopopopopopooppopopopopopopopoop')
+                        print ('')
                         print ('')
 
 
 
-                    # if (num_batches%20 ==0 and num_batches!=0):
-                    #     print ('Evaluating cross validation dataset ')
-                    #     for cdoc_no in cdoc:
-                    #         with open(self.valid_batch_dir+'batch'+str(cdoc_no)+'.pickle', 'rb') as f1:
-                    #             dataset = pickle.load(f)
-                            
-                    #             batch_valid_dataset = dataset['batch_valid_dataset']
-                    #             batch_valid_labels = dataset['batch_valid_labels']
-
-                    #             # We use the same weights that have been involved with the change on the cross validation dataset
-                    #             feed_dict= {graph_dict['x']: batch_valid_dataset, 
-                    #                         graph_dict['y']: batch_valid_labels,
-                    #                         graph_dict['init_state'] : new_hid_layer_state}
-
-                    #             bs, nwst, loss, opt, tp = sess.run([graph_dict['batch_size'],
-                    #                                     graph_dict['new_state'],
-                    #                                     graph_dict['loss_CE'],
-                    #                                     graph_dict['optimizer'],
-                    #                                     graph_dict['training_prediction']], 
-                    #                                     feed_dict=feed_dict)
-
-                    #             print ('accuracy \n', acc)
-                    #             print ('')
-                    #             print ('popopopopopopopoop')
-                    #             print ('')
+obj_Train = Train()
+graph_dict =  dynamic_RNN_model(vocab_size = 7)
+obj_Train.train_network(graph_dict)
 
         
-obj_Train = Train()
-# print (obj_Train.vocab_size)
-# print (obj_Model.vocab_size)
-graph_dict =  dynamic_RNN_model(num_hid_units = 3,
-                                vocab_size = obj_Train.vocab_size,
-                                momentum = 0.9,
-                                learning_rate = 0.01)
-# print (graph_dict['batch_size'])
-obj_Train.train_network(graph_dict, num_batches=2)
+# obj_Train = Train()
+# # print (obj_Train.vocab_size)
+# # print (obj_Model.vocab_size)
+# graph_dict =  dynamic_RNN_model(num_hid_units = 3,
+#                                 vocab_size = obj_Train.vocab_size,
+#                                 momentum = 0.9,
+#                                 learning_rate = 0.01)
+# # print (graph_dict['batch_size'])
+# obj_Train.train_network(graph_dict, num_batches=2)
 
 
